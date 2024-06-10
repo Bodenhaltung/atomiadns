8f55c6465fcb40be7ddee45b7f446791580bd746
Jimmy Bergman <jimmy@sigint.se>
2024-06-10 14:05:20 +0200

    🐛 Reload all tsigkeys on full slavezone reload

--- server/schema/reloadallslavezones.sql
+++ server/schema/reloadallslavezones.sql
@@ -2,4 +2,6 @@ CREATE OR REPLACE FUNCTION ReloadAllSlaveZones () RETURNS void AS $$
 BEGIN
 	INSERT INTO slavezone_change (nameserver_id, zone)
 	SELECT nameserver.id, slavezone.name FROM nameserver, slavezone WHERE nameserver.nameserver_group_id = slavezone.nameserver_group_id;
+	INSERT INTO tsigkey_change (nameserver_id, tsigkey_name)
+	SELECT n.id, k.name FROM tsigkey k INNER JOIN nameserver n ON k.nameserver_group_id = n.nameserver_group_id;
 END; $$ LANGUAGE plpgsql;
